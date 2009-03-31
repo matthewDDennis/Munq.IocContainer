e2aae8bbd3a1a11f33e4644622dab665fefe128a
mdennis_cp <SND\mdennis_cp>
2009-03-31 01:23:31 +0000

    Initial Checkin. Munq.DI handles unnamed registrations and resolves. Always creates new instance.

---
+++ Munq.DI.Tests/Foo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Munq.DI.Tests
+{
+    public interface IFoo
+    {
+        string Name { get; }
+    }
+
+    public class Foo1: IFoo
+    {
+        #region IFoo Members
+
+        public string Name
+        {
+            get { return "Foo1"; }
+        }
+
+        #endregion
+    }
+
+    public class Foo2 : IFoo
+    {
+        #region IFoo Members
+
+        public string Name
+        {
+            get { return "Foo2"; }
+        }
+
+        #endregion
+    }
+
+    public interface IBar
+    {
+    }
+
+    public class Bar1 : IBar
+    {
+    }
+
+    public class Bar2 : IBar
+    {
+    }
+}
---
+++ Munq.DI/IRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Munq.DI
+{
+    public interface IRegistration
+    {
+    }
+
+    public interface IRegistrationKey
+    {
+        bool Equals(object obj);
+        int GetHashCode();
+    }
+}
