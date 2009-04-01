f38ca48233a577e178b42c8d192fce346e4d08fb
mdennis_cp <matthew.dennis@rogers.com>
2009-04-01 16:38:16 +0000

    Added Session, Request, and start of Cached Lifetime managers added MvcFakes and added Items property to FakeHttpContext

---
+++ Munq/Munq.DI/LifetimeManagers/SessionLifetime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.SessionState;
+
+namespace Munq.DI.LifetimeManagers
+{
+    public class SessionLifetime<TType> : ILifetimeManager<TType> where TType : class
+    {
+        HttpSessionStateBase _session = null;
+        #region ILifetimeManager<TType> Members
+
+        public TType GetInstance(Container container, Registration<TType> reg)
+        {
+            HttpSessionStateBase session;
+            if (System.Web.HttpContext.Current != null)
+            {
+                session = new HttpSessionStateWrapper(HttpContext.Current.Session);
+            }
+            else
+            {
+                session = _session;
+            }
+
+            TType instance = (TType)session[reg.ID.ToString()];
+            if (instance == null)
+            {
+                instance = reg.Factory(container);
+                session[reg.ID.ToString()] = instance;
+            }
+            return instance;
+        }
+
+        #endregion
+
+        // only used for testing.  Has no effect when in web application
+        public void SetContext(HttpContextBase context)
+        {
+            _session = context.Session;
+        }
+    }
+}
