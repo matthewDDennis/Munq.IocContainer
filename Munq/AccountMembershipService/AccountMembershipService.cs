99669f9198e9d57ef40d802afc651bda0ad6b89a
mdennis_cp <matthew.dennis@rogers.com>
2009-11-04 00:27:36 +0000

    removed dependency implementations in AccountController.cs to external projects

---
+++ Munq/AccountMembershipService/AccountMembershipService.cs
@@ -0,0 +1,47 @@
+﻿// Article3
+using System;
+using System.Web.Security;
+
+using FinalApp.Interfaces;
+
+namespace FinalApp
+{
+    public class AccountMembershipService : IMembershipService
+    {
+        private MembershipProvider _provider;
+
+        public AccountMembershipService(MembershipProvider provider)
+        {
+            if (provider == null)
+                throw new ArgumentNullException("provider");
+                
+            _provider = provider;
+        }
+
+        public int MinPasswordLength
+        {
+            get
+            {
+                return _provider.MinRequiredPasswordLength;
+            }
+        }
+
+        public bool ValidateUser(string userName, string password)
+        {
+            return _provider.ValidateUser(userName, password);
+        }
+
+        public MembershipCreateStatus CreateUser(string userName, string password, string email)
+        {
+            MembershipCreateStatus status;
+            _provider.CreateUser(userName, password, email, null, null, true, null, out status);
+            return status;
+        }
+
+        public bool ChangePassword(string userName, string oldPassword, string newPassword)
+        {
+            MembershipUser currentUser = _provider.GetUser(userName, true /* userIsOnline */);
+            return currentUser.ChangePassword(oldPassword, newPassword);
+        }
+    }
+}
---
+++ Munq/FinalApp/Global.asax.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Munq.DI;
+using Munq.MVC;
+using FinalApp.Controllers;
+
+using FinalApp.Interfaces;
+using System.Web.Security;
+
+namespace FinalApp
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static Container IOC { get; private set; }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            InitializeIOC();
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void InitializeIOC()
+        {
+            // Create the IOC container
+            IOC = new Container();
+
+            // Create the Default Factory
+            var controllerFactory = new MunqControllerFactory(IOC);
+
+            // set the controller factory
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+
+            // Register the dependencies
+            // Article3
+            // The dependencies to the concrete implementation should be externalized
+            IOC.Register<IFormsAuthentication>(ioc => new FormsAuthenticationService());
+            IOC.Register<MembershipProvider>(ioc => Membership.Provider);
+            IOC.Register<IMembershipService>(ioc => new AccountMembershipService(ioc.Resolve<MembershipProvider>()));
+
+            // Register the Controllers
+            IOC.Register<IController>("Home", ioc => new HomeController());
+            IOC.Register<IController>("Account",
+                    ioc => new AccountController(ioc.Resolve<IFormsAuthentication>(),
+                                                  ioc.Resolve<IMembershipService>())
+            );
+
+
+        }
+    }
+}
