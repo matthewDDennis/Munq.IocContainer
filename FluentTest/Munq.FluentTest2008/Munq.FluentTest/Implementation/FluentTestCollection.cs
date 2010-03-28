1062c8cc09e2e542612e6fc2a3db670b863c79cd
mdennis_cp <SND\mdennis_cp>
2010-03-28 02:38:48 +0000

    remove collection methods that take string msg parameter

--- FluentTest/Munq.FluentTest2008/Munq.FluentTest/Implementation/FluentTestCollection.cs
+++ FluentTest/Munq.FluentTest2008/Munq.FluentTest/Implementation/FluentTestCollection.cs
@@ -21,21 +21,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.IsTheSameCollectionAs(ICollection objectToCompare, string msg)
-        {
-            Assert.AreSame(CollectionToTest, objectToCompare, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.IsNotTheSameCollectionAs(ICollection objectToCompare)
         {
             Assert.AreNotSame(ObjectToTest, objectToCompare);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.IsNotTheSameCollectionAs(ICollection objectToCompare, string msg)
-        {
-            Assert.AreNotSame(CollectionToTest, objectToCompare, msg);
             return this;
         }
 
@@ -45,21 +33,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.IsAnInstanceOfType(Type type, string msg)
-        {
-            Assert.IsInstanceOfType(CollectionToTest, type, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.IsNotAnInstanceOfType(Type type)
         {
             Assert.IsNotInstanceOfType(ObjectToTest, type);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.IsNotAnInstanceOfType(Type type, string msg)
-        {
-            Assert.IsNotInstanceOfType(CollectionToTest, type, msg);
             return this;
         }
 
@@ -68,22 +44,10 @@ namespace Munq.FluentTest
             Assert.IsNotNull(ObjectToTest);
             return this;
         }
-
-        IFluentCollectionTest IFluentCollectionTest.IsNotNull(string msg)
-        {
-            Assert.IsNotNull(CollectionToTest, msg);
-            return this;
-        }
-        
+      
         IFluentCollectionTest IFluentCollectionTest.AllItemsAreInstancesOfType(Type type)
         {
             CollectionAssert.AllItemsAreInstancesOfType(CollectionToTest, type);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.AllItemsAreInstancesOfType(Type type, string msg)
-        {
-            CollectionAssert.AllItemsAreInstancesOfType(CollectionToTest, type, msg);
             return this;
         }
 
@@ -93,21 +57,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.AllItemsAreNotNull(string msg)
-        {
-            CollectionAssert.AllItemsAreNotNull(CollectionToTest, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.AllItemsAreUnique()
         {
             CollectionAssert.AllItemsAreUnique(CollectionToTest);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.AllItemsAreUnique(string msg)
-        {
-            CollectionAssert.AllItemsAreUnique(CollectionToTest, msg);
             return this;
         }
 
@@ -117,21 +69,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.IsEqualTo(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.AreEqual(CollectionToTest, ObjectToCompare, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.IsNotEqualTo(ICollection ObjectToCompare)
         {
             CollectionAssert.AreNotEqual(CollectionToTest, ObjectToCompare);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.IsNotEqualTo(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.AreNotEqual(CollectionToTest, ObjectToCompare, msg);
             return this;
         }
 
@@ -141,21 +81,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.IsEquivalentTo(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.AreEquivalent(CollectionToTest, ObjectToCompare, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.IsNotEquivalentTo(ICollection ObjectToCompare)
         {
             CollectionAssert.AreNotEquivalent(CollectionToTest, ObjectToCompare);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.IsNotEquivalentTo(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.AreNotEquivalent(CollectionToTest, ObjectToCompare, msg);
             return this;
         }
 
@@ -165,21 +93,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.Contains(object ObjectToCompare, string msg)
-        {
-            CollectionAssert.Contains(CollectionToTest, ObjectToCompare, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.DoesNotContain(object ObjectToCompare)
         {
             CollectionAssert.DoesNotContain(CollectionToTest, ObjectToCompare);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.DoesNotContain(object ObjectToCompare, string msg)
-        {
-            CollectionAssert.DoesNotContain(CollectionToTest, ObjectToCompare, msg);
             return this;
         }
 
@@ -189,21 +105,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.IsASubsetOf(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.IsSubsetOf(CollectionToTest, ObjectToCompare, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.IsNotASubsetOf(ICollection ObjectToCompare)
         {
             CollectionAssert.IsNotSubsetOf(CollectionToTest, ObjectToCompare);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.IsNotASubsetOf(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.IsNotSubsetOf(CollectionToTest, ObjectToCompare, msg);
             return this;
         }
 
@@ -213,21 +117,9 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.IsASupersetOf(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.IsSubsetOf(ObjectToCompare, CollectionToTest, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.IsNotASupersetOf(ICollection ObjectToCompare)
         {
             CollectionAssert.IsNotSubsetOf(ObjectToCompare, CollectionToTest);
-            return this;
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.IsNotASupersetOf(ICollection ObjectToCompare, string msg)
-        {
-            CollectionAssert.IsNotSubsetOf(ObjectToCompare, CollectionToTest, msg);
             return this;
         }
 
@@ -237,35 +129,17 @@ namespace Munq.FluentTest
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.CountIsEqualTo(int numberOfItems, string msg)
-        {
-            Assert.IsTrue(CollectionToTest.Count == numberOfItems, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.CountIsNotEqualTo(int numberOfItems)
         {
             Assert.IsTrue(CollectionToTest.Count != numberOfItems);
             return this;
         }
 
-        IFluentCollectionTest IFluentCollectionTest.CountIsNotEqualTo(int numberOfItems, string msg)
-        {
-            Assert.IsTrue(CollectionToTest.Count != numberOfItems, msg);
-            return this;
-        }
-  
         IFluentCollectionTest IFluentCollectionTest.CountIsGreaterThan(int numberOfItems)
         {
             Assert.IsTrue(CollectionToTest.Count > numberOfItems);
             return this;
             throw new NotImplementedException();
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.CountIsGreaterThan(int numberOfItems, string msg)
-        {
-            Assert.IsTrue(CollectionToTest.Count > numberOfItems, msg);
-            return this;
         }
 
         IFluentCollectionTest IFluentCollectionTest.CountIsGreaterThanOrEqualTo(int numberOfItems)
@@ -275,12 +149,6 @@ namespace Munq.FluentTest
             throw new NotImplementedException();
         }
 
-        IFluentCollectionTest IFluentCollectionTest.CountIsGreaterThanEqualTo(int numberOfItems, string msg)
-        {
-            Assert.IsTrue(CollectionToTest.Count >= numberOfItems, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.CountIsLessThan(int numberOfItems)
         {
             Assert.IsTrue(CollectionToTest.Count < numberOfItems);
@@ -288,23 +156,11 @@ namespace Munq.FluentTest
             throw new NotImplementedException();
         }
 
-        IFluentCollectionTest IFluentCollectionTest.CountIsLessThan(int numberOfItems, string msg)
-        {
-            Assert.IsTrue(CollectionToTest.Count < numberOfItems, msg);
-            return this;
-        }
-
         IFluentCollectionTest IFluentCollectionTest.CountIsLessThanOrEqualTo(int numberOfItems)
         {
             Assert.IsTrue(CollectionToTest.Count <= numberOfItems);
             return this;
             throw new NotImplementedException();
-        }
-
-        IFluentCollectionTest IFluentCollectionTest.CountIsLessThanEqualTo(int numberOfItems, string msg)
-        {
-            Assert.IsTrue(CollectionToTest.Count <= numberOfItems, msg);
-            return this;
         }
         #endregion
     }
--- FluentTest/Munq.FluentTest2008/Munq.FluentTest/Interfaces/IFluentCollectionTest.cs
+++ FluentTest/Munq.FluentTest2008/Munq.FluentTest/Interfaces/IFluentCollectionTest.cs
@@ -20,23 +20,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest IsNotNull();
 
         /// <summary>
-        /// The assertion fails if the collection is null.
-        /// </summary>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsNotNull(string msg);
-
-        /// <summary>
         /// The assertion fails if the type of the collection is not the specified type.
         /// </summary>
         /// <param name="type">The Type to compare against.</param>
         IFluentCollectionTest IsAnInstanceOfType(Type type);
-
-        /// <summary>
-        /// The assertion fails if the type of the collection is not the specified type.
-        /// </summary>
-        /// <param name="type">The Type to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsAnInstanceOfType(Type type, string msg);
 
         /// <summary>
         /// The assertion fails if the type of the collection is the specified type.
@@ -45,25 +32,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest IsNotAnInstanceOfType(Type type);
 
         /// <summary>
-        /// The assertion fails if the type of the collection is the specified type.
-        /// </summary>
-        /// <param name="type">The Type to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsNotAnInstanceOfType(Type type, string msg);
-
-
-        /// <summary>
         /// The assertion fails if the collection is not the nosame collection as the specified collection.
         /// </summary>
         /// <param name="objToCompare">The object to compare.</param>
         IFluentCollectionTest IsTheSameCollectionAs(ICollection objToCompare);
-
-        /// <summary>
-        /// The assertion fails if the collection is not the nosame collection as the specified collection.
-        /// </summary>
-        /// <param name="objToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsTheSameCollectionAs(ICollection objToCompare, string msg);
 
         /// <summary>
         /// The assertion fails if the collection is the same collection as the specified collection.
@@ -72,24 +44,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest IsNotTheSameCollectionAs(ICollection objToCompare);
 
         /// <summary>
-        /// The assertion fails if the collection is the same collection as the specified collection.
-        /// </summary>
-        /// <param name="objToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsNotTheSameCollectionAs(ICollection objToCompare, string msg);
-
-        /// <summary>
         /// The assertion fails if any of the objects in the collection are not instance of, or derived from, the specified type.
         /// </summary>
         /// <param name="type">The Type to compare against.</param>
         IFluentCollectionTest AllItemsAreInstancesOfType(Type type);
-
-        /// <summary>
-        /// The assertion fails if any of the objects in the collection are not instance of, or derived from, the specified type.
-        /// </summary>
-        /// <param name="type">The Type to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest AllItemsAreInstancesOfType(Type type, string msg);
 
         /// <summary>
         /// The assertion fails if any of the objects in the collection are null.
@@ -97,33 +55,15 @@ namespace Munq.FluentTest
         IFluentCollectionTest AllItemsAreNotNull();
 
         /// <summary>
-        /// The assertion fails if any of the objects in the collection are null.
-        /// </summary>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest AllItemsAreNotNull(string msg);
-
-        /// <summary>
         /// The assertion fails if any of the any objects in the collection are duplicates.
         /// </summary>
         IFluentCollectionTest AllItemsAreUnique();
-
-        /// <summary>
-        /// The assertion fails if any of the any objects in the collection are duplicates.
-        /// </summary>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest AllItemsAreUnique(string msg);
 
         /// <summary>
         /// The assertion fails if the collections are not equal.
         /// </summary>
         /// <param name="ObjectToCompare">The object to compare.</param>
         IFluentCollectionTest IsEqualTo(ICollection ObjectToCompare);
-        /// <summary>
-        /// The assertion fails if the collections are not equal.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsEqualTo(ICollection ObjectToCompare, string msg);
 
         /// <summary>
         /// The assertion fails if the collections are equal.
@@ -132,24 +72,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest IsNotEqualTo(ICollection ObjectToCompare);
 
         /// <summary>
-        /// The assertion fails if the collections are equal.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsNotEqualTo(ICollection ObjectToCompare, string msg);
-
-        /// <summary>
         /// The assertion fails if the collections are not eqivalent
         /// </summary>
         /// <param name="ObjectToCompare">The object to compare.</param>
         IFluentCollectionTest IsEquivalentTo(ICollection ObjectToCompare);
-
-        /// <summary>
-        /// The assertion fails if the collections are not eqivalent
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsEquivalentTo(ICollection ObjectToCompare, string msg);
 
         /// <summary>
         /// The assertion fails if the collections are eqivalent
@@ -158,24 +84,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest IsNotEquivalentTo(ICollection ObjectToCompare);
 
         /// <summary>
-        /// The assertion fails if the collections are eqivalent
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsNotEquivalentTo(ICollection ObjectToCompare, string msg);
-
-        /// <summary>
         /// The assertion fails if  the collection does not contain the specified object.
         /// </summary>
         /// <param name="ObjectToCompare">The object to compare.</param>
         IFluentCollectionTest Contains(object ObjectToCompare);
-
-        /// <summary>
-        /// The assertion fails if any of the any objects in the collection are duplicates.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest Contains(object ObjectToCompare, string msg);
 
         /// <summary>
         /// The assertion fails if  the collection does not contain the specified object.
@@ -184,24 +96,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest DoesNotContain(object ObjectToCompare);
 
         /// <summary>
-        /// The assertion fails if  the collection does not contain the specified object.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest DoesNotContain(object ObjectToCompare, string msg);
-
-        /// <summary>
         /// The assertion fails if the type of the collection is  a subset of the specified collection.
         /// </summary>
         /// <param name="ObjectToCompare">The object to compare.</param>
         IFluentCollectionTest IsNotASubsetOf(ICollection ObjectToCompare);
-
-        /// <summary>
-        /// The assertion fails if the type of the collection is  a subset of the specified collection.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsNotASubsetOf(ICollection ObjectToCompare, string msg);
 
         /// <summary>
         /// The assertion fails if the type of the collection is not a subset of the specified collection.
@@ -210,24 +108,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest IsASubsetOf(ICollection ObjectToCompare);
 
         /// <summary>
-        /// The assertion fails if the type of the collection is not a subset of the specified collection.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsASubsetOf(ICollection ObjectToCompare, string msg);
-
-        /// <summary>
         /// The assertion fails if the type of the collection is a superset of the specified collection.
         /// </summary>
         /// <param name="ObjectToCompare">The object to compare.</param>
         IFluentCollectionTest IsNotASupersetOf(ICollection ObjectToCompare);
-
-        /// <summary>
-        /// The assertion fails if the type of the collection is a superset of the specified collection.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsNotASupersetOf(ICollection ObjectToCompare, string msg);
 
         /// <summary>
         /// The assertion fails if the type of the collection is not a superset of the specified collection.
@@ -236,24 +120,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest IsASupersetOf(ICollection ObjectToCompare);
 
         /// <summary>
-        /// The assertion fails if the type of the collection is not a superset of the specified collection.
-        /// </summary>
-        /// <param name="ObjectToCompare">The object to compare.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest IsASupersetOf(ICollection ObjectToCompare, string msg);
-
-        /// <summary>
         /// The assertion fails if  the number of items in the  collection is not equal to the specified number.
         /// </summary>
         /// <param name="numberOfItems">The number to compare against.</param>
         IFluentCollectionTest CountIsEqualTo(int numberOfItems);
-
-        /// <summary>
-        /// The assertion fails if  the number of items in the  collection is not equal to the specified number.
-        /// </summary>
-        /// <param name="numberOfItems">The number to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest CountIsEqualTo(int numberOfItems, string msg);
 
         /// <summary>
         /// The assertion fails if  the number of items in the  collection is equal to  the specified number.
@@ -262,24 +132,10 @@ namespace Munq.FluentTest
         IFluentCollectionTest CountIsNotEqualTo(int numberOfItems);
 
         /// <summary>
-        /// The assertion fails if  the number of items in the  collection is equal to  the specified number.
-        /// </summary>
-        /// <param name="numberOfItems">The number to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest CountIsNotEqualTo(int numberOfItems, string msg);
-
-        /// <summary>
         /// The assertion fails if  the number of items in the  collection is not greater than  the specified number.
         /// </summary>
         /// <param name="numberOfItems">The number to compare against.</param>
         IFluentCollectionTest CountIsGreaterThan(int numberOfItems);
-
-        /// <summary>
-        /// The assertion fails if  the number of items in the  collection is not greater than  the specified number.
-        /// </summary>
-        /// <param name="numberOfItems">The number to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest CountIsGreaterThan(int numberOfItems, string msg);
 
         /// <summary>
         /// The assertion fails if  the number of items in the  collection is less  than  the specified number.
@@ -288,36 +144,15 @@ namespace Munq.FluentTest
         IFluentCollectionTest CountIsGreaterThanOrEqualTo(int numberOfItems);
 
         /// <summary>
-        /// The assertion fails if  the number of items in the  collection is less  than  the specified number.
-        /// </summary>
-        /// <param name="numberOfItems">The number to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest CountIsGreaterThanEqualTo(int numberOfItems, string msg);
-
-        /// <summary>
         /// The assertion fails if  the number of items in the  collection is not less  than  the specified number.
         /// </summary>
         /// <param name="numberOfItems">The number to compare against.</param>
         IFluentCollectionTest CountIsLessThan(int numberOfItems);
 
         /// <summary>
-        /// The assertion fails if  the number of items in the  collection is not less  than  the specified number.
-        /// </summary>
-        /// <param name="numberOfItems">The number to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest CountIsLessThan(int numberOfItems, string msg);
-
-        /// <summary>
         /// The assertion fails if  the number of items in the  collection is greater than  the specified number.
         /// </summary>
         /// <param name="numberOfItems">The number to compare against.</param>
         IFluentCollectionTest CountIsLessThanOrEqualTo(int numberOfItems);
-
-        /// <summary>
-        /// The assertion fails if  the number of items in the  collection is greater than  the specified number.
-        /// </summary>
-        /// <param name="numberOfItems">The number to compare against.</param>
-        /// <param name="msg">The message to display.</param>
-        IFluentCollectionTest CountIsLessThanEqualTo(int numberOfItems, string msg);
     }
 }
