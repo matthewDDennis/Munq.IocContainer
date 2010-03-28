6e48fda2b9a44f77b11a1b4910be96cc8acab98a
mdennis_cp <matthew.dennis@rogers.com>
2010-03-28 15:10:14 +0000

    more tests

--- FluentTest/Munq.FluentTest2008/Munq.FluentTest.UnitTests/String/IFluentTestStringMatchesTest.cs
+++ FluentTest/Munq.FluentTest2008/Munq.FluentTest.UnitTests/String/IFluentTestStringMatchesTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Munq.FluentTest;
 
@@ -11,10 +12,53 @@ namespace Munq.FluentTest.UnitTests
     [TestClass()]
     public class IFluentTestStringMatchesTest
     {
+        private const string testString = "May the Force be with you.";
          #region Matches
+        [TestMethod]
+        public void MatchesFailsForNullCompareRegEx()
+        {
+            Verify.TheExpectedException(Verify.FailExceptionType).IsThrownWhen(
+                () => Verify.That(testString).IsAString().Matches(null)
+            );
+        }
+
+        [TestMethod]
+        public void MatchesFailsForNonMatchingRegEx()
+        {
+            Verify.TheExpectedException(Verify.FailExceptionType).IsThrownWhen(
+                () => Verify.That(testString).IsAString().Matches(new Regex(@"\d\d\d"))
+            );
+        }
+
+        [TestMethod]
+        public void MatchesPassesForMatchingRegEx()
+        {
+            Verify.That(testString).IsAString().Matches(new Regex(@".*F.*"));
+        }
         #endregion
 
         #region DoesNotMatch
+        [TestMethod]
+        public void DoesNotMatchFailsForNullCompareRegEx()
+        {
+            Verify.TheExpectedException(Verify.FailExceptionType).IsThrownWhen(
+                () => Verify.That(testString).IsAString().DoesNotMatch(null)
+            );
+        }
+
+        [TestMethod]
+        public void DoesNotMatchFailsForMatchingRegEx()
+        {
+            Verify.TheExpectedException(Verify.FailExceptionType).IsThrownWhen(
+                () => Verify.That(testString).IsAString().DoesNotMatch(new Regex(@"^May.*\.$"))
+            );
+        }
+
+        [TestMethod]
+        public void DoesNotMatchPassesForNonMatchingRegEx()
+        {
+            Verify.That(testString).IsAString().DoesNotMatch(new Regex("Jedi"));
+        }
         #endregion
 
 
