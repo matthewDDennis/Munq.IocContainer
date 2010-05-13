b330b849de71fc7069d0669b01e64f73dbb42a7e
mdennis_cp <SND\mdennis_cp>
2010-05-13 01:00:26 +0000

    converted to VS2010

--- IocContainer/Munq.Interfaces/IInstanceCreator.cs
+++ IocContainer/Munq.Interfaces/IInstanceCreator.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 namespace Munq
 {
-    public enum ContainerCaching
-    {
-        InstanceCachedInContainer,
-        InstanceNotCachedInContainer
-    }
-
     public interface IInstanceCreator
     {
         string Key { get; }
         object CreateInstance(ContainerCaching containerCache);
     }
-}
+    
+public enum ContainerCaching
+    {
+        InstanceCachedInContainer,
+        InstanceNotCachedInContainer
+    }}
