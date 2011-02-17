ce7cbfa16ec9a3142d98027d6eb2002279dd17af
mdennis_cp <matthew.dennis@rogers.com>
2011-02-17 03:46:20 +0000

    added MunqCommonServiceLocator implementation

---
+++ IocContainer/Munq.CommonServiceLocator/MunqCommonServiceLocator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Munq.CommonServiceLocator
+{
+	public class MunqCommonServiceLocator : IServiceLocator
+	{
+		private readonly Munq.IDependencyResolver _resolver;
+
+		public MunqCommonServiceLocator(Munq.IDependencyResolver _munqResolver)
+		{
+			_resolver = _munqResolver;
+		}
+
+		public IEnumerable<TService> GetAllInstances<TService>()
+		{
+			return GetAllInstances(typeof(TService)).Cast<TService>();
+		}
+
+		public IEnumerable<object> GetAllInstances(Type serviceType)
+		{
+			return _resolver.ResolveAll(serviceType);
+		}
+
+		public TService GetInstance<TService>(string key)
+		{
+			return (TService)GetInstance(typeof(TService), key);
+		}
+
+		public TService GetInstance<TService>()
+		{
+			return (TService)GetInstance(typeof(TService), null);
+		}
+
+		public object GetInstance(Type serviceType, string key)
+		{
+			try
+			{
+				return _resolver.Resolve(key, serviceType);
+			}
+			catch
+			{
+				throw new ActivationException();
+			}
+		}
+
+		public object GetInstance(Type serviceType)
+		{
+			return GetInstance(serviceType, null);
+		}
+	
+		public object  GetService(Type serviceType)
+		{
+ 				try
+ 				{
+ 				return GetInstance(serviceType);
+ 				}
+ 				catch (ActivationException)
+ 				{
+ 					return null;
+ 				}
+		}
+	}
+}
