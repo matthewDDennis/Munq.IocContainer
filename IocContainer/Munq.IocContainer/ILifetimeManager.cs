77720a961d657c9b3ad255dde9558b389c0c2046
mdennis_cp <matthew.dennis@rogers.com>
2011-03-05 20:12:25 +0000

    Moved Interfaces into IocContainer.  Most users of the Interfaces will use either the Container or the LifetimeManagers, so no benefit to two dlls.

---
+++ IocContainer/Munq.IocContainer/ILifetimeManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Munq
+{
+    public interface ILifetimeManager
+    {
+        object GetInstance(IRegistration creator);
+        void   InvalidateInstanceCache(IRegistration registration);
+    }
+}
